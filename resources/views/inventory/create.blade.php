db3718cc8c655b2152ec6c1146be6468cfdbf475
jacuba98 <jacuba98456@gmail.com>
2023-07-02 22:01:59 -0500

    Diseño de Nuevo Registro

--- resources/views/inventory/create.blade.php
+++ resources/views/inventory/create.blade.php
@@ -1,23 +1,32 @@
-<!-- create.blade.php -->
-<h1>Agregar nuevo elemento de inventario</h1>
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h5>Nuevo Registro</h5>
 
-@if($errors->any())
-    <ul>
-        @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
+                    @if($errors->any())
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
 
-<form action="{{ route('inventory.store') }}" method="POST">
-    @csrf
-    <div>
-        <label for="name">Nombre:</label>
-        <input type="text" name="name" id="name" value="{{ old('name') }}">
+                    <form action="{{ route('inventory.store') }}" method="POST" class="form-gp">
+                        @csrf
+                        <div>
+                            <label for="name" class="label-gp">Nombre:</label>
+                            <input type="text" name="name" id="name" value="{{ old('name') }}" class="input-gp">
+                        </div>
+                        <div>
+                            <label for="equipo" class="label-gp">Equipo:</label>
+                            <input type="text" name="equipo" id="equipo" value="{{ old('equipo') }}" class="input-gp">
+                        </div>
+                        <button type="submit" class="button-gp-create">Guardar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <div>
-        <label for="equipo">Equipo:</label>
-        <input type="text" name="equipo" id="equipo" value="{{ old('equipo') }}">
-    </div>
-    <button type="submit">Guardar</button>
-</form>
+</x-app-layout>
