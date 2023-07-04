5fbe23742a4cc14ec9d5cb9f69f3066530bd39b6
jacuba98 <jacuba98456@gmail.com>
2023-07-04 14:16:20 -0500

    Diseño de vista Show

--- resources/views/inventory/show.blade.php
+++ resources/views/inventory/show.blade.php
@@ -1,8 +1,42 @@
-<!-- show.blade.php -->
-<h1>Detalles del elemento de inventario</h1>
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <!-- show.blade.php -->
+                    <h1>Detalles del elemento de inventario</h1>
 
-<p>ID: {{ $inventory->id }}</p>
-<p>Nombre: {{ $inventory->name }}</p>
-<p>Cantidad: {{ $inventory->equipo }}</p>
+                    <a href="{{ route('inventory.index') }}">Volver</a>
 
-<a href="{{ route('inventory.index') }}">Volver</a>
+                    <table class="table-container">
+                        <thead>
+                            <tr class="white-text">
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Equipo</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{{ $inventory->id }}</td>
+                                <td>{{ $inventory->name }}</td>
+                                <td>{{ $inventory->equipo }}</td>
+                                <td>
+                                    <a href="{{ route('inventory.edit', $inventory->id) }}" class="button-gp edit" class="button">Edit</a>
+
+                                    <form action="{{ route('inventory.destroy', $inventory->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="button-gp delete">Eliminar</button>
+                                    </form>
+                                  </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
